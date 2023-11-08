daa6d24b69570a63cd6538989c2a130d56cffe80
flaviogcunha <flaviogoncalves678@gmail.com>
2023-11-08 20:38:04 -0300

    fla

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template ('home.html')
-@app.route('/contato')
-def contato():
-    return render_template ('contato.html')
-@app.route('/quemsomos')
-def quemsomos():    
-    return render_template ('quemsomos.html')
+@app.route('/marcantes')
+def marcantes():
+    return render_template ('marcantes.html')
+@app.route('/titulos')
+def titulos():    
+    return render_template ('titulos.html')
